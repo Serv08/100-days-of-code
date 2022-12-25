8158187b2a60b5122527c8b4727ae4b9859eda74
Servin Sanchez <93779413+Serv08@users.noreply.github.com>
2022-12-26 02:02:45 +0800

    Update 03-DVD-Logo.py

--- BigBookSmallPythonProjects/03-DVD-Logo.py
+++ BigBookSmallPythonProjects/03-DVD-Logo.py
@@ -1,5 +1,4 @@
 import sys, random, time
-# import bext
 
 try: 
     import bext
@@ -34,7 +33,7 @@ def main():
 
     logos = []
     for i in range(NUMBER_OF_LOGOS):
-        logos.append({COLOR: random.choice(COLORS), X: random.randint(1, WIDTH - 4), Y: random.randint(1, HEIGHT), DIR: random.randint(DIRECTIONS)})
+        logos.append({COLOR: random.choice(COLORS), X: random.randint(1, WIDTH - 4), Y: random.randint(1, HEIGHT - 4), DIR: random.choice(DIRECTIONS)})
         if logos[-1][X] % 2 == 1:
             logos[-1][X] -= 1
 
@@ -42,7 +41,7 @@ def main():
     while True:
         for logo in logos:
             bext.goto(logo[X], logo[Y])
-            print('     ', end='')
+            print('  ', end='')
 
             originalDirection = logo[DIR]
 
